06e39bb05b16ca4e1c3bd2cf2d99a7d6a4d3f212
Alexej Melnyk <melnyk.alexej@gmail.com>
2022-07-18 00:56:56 +0300

    1 задача без ЕСП

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCards(galleryItems);
+console.log(createGalleryCards(galleryItems));
+
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+
+function createGalleryCards(galleryItems) { 
+
+
+    
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      
+    />
+  </a>
+</div> 
+    `;
+    }).join('');
+    
+
+    
+};
+
+
+galleryEl.addEventListener('click', onClickGalleryFoto);
+
+function onClickGalleryFoto(event) { 
+    
+    
+    if (!event.target.classList.contains('gallery__image')) { 
+        return;
+    }
+event.preventDefault();
+
+   const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+    
+    instance.show();
+
+
+};
+
+
+
+
+
+window.addEventListener('keydown', onEscKeyPress);
+
+
+function onEscKeyPress(event) { 
+   console.log(event)
+    }
+    
+    
